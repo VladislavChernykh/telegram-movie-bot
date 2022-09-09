e02eb4fffb8211eb688e495b99c4bb4adcbe8aee
Chernykh Vladislav <vlad.chernykh@mail.ru>
2022-09-09 14:07:28 +0200

    Simplify the logic

--- bot.mjs
+++ bot.mjs
@@ -4,6 +4,6 @@ const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
 bot.on('text', msg => msg.reply.text(msg.text))
 
-bot.on('/start', (msg) => { msg.reply.photo("https://picsum.photos/1000")});
+bot.on('/start', (msg) => msg.reply.photo("https://picsum.photos/1000"));
 
 export default bot

72960f6b9f42078d0a57f704cb92c7f0db2a8452
Vladislav Chernykh <vchernykh@innoseti.ru>
2022-09-09 18:10:23 +0200

    Add new actions

--- bot.mjs
+++ bot.mjs
@@ -2,8 +2,18 @@ import TeleBot from "telebot"
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
-bot.on('text', msg => msg.reply.text(msg.text))
+bot.on('text', msg => msg.text.startsWith("/") ? null : msg.reply.text(msg.text))
 
 bot.on('/start', (msg) => msg.reply.photo("https://picsum.photos/1000"));
+
+var testArr = []
+bot.on('/env', (msg) => msg.reply.text(process.env.VERCEL_ENV));
+
+bot.on(/^\/say (.+)$/, (msg, props) => {
+    console.log(props)
+    const text = props.match[1];
+    testArr.push(text)
+    return bot.sendMessage(msg.from.id, text, { replyToMessage: msg.message_id });
+});
 
 export default bot

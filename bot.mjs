698acf7616f90542ccdca7b6407066a43c5da253
Chernykh Vladislav <vlad.chernykh@mail.ru>
2022-09-09 14:01:11 +0200

    Add /start endpoint

--- bot.mjs
+++ bot.mjs
@@ -4,4 +4,6 @@ const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
 bot.on('text', msg => msg.reply.text(msg.text))
 
+bot.on('/start', (msg) => { msg.reply.photo("https://picsum.photos/1000")});
+
 export default bot
